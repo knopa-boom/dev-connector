f8c5367b36492f0eccc0d171a657c3c37ed6eb7a
Anastasiia Andriushchenko <aandriushchenko@atlassian.com>
2022-03-22 13:46:10 +0200

    user registration

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true
+    mongoose.connect(db, {
+      useNewUrlParser: true,
     });
 
     console.log("MongoDB Connected");
