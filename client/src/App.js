1e0c2a41da3f02957c071408f45ba1f15aff0606
Anastasiia Andriushchenko <aandriushchenko@atlassian.com>
2022-04-04 00:14:59 +0300

    redux store added

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./App.css";
 
 const App = () => (
-  <Router>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route exact path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  </Provider>
 );
 
 export default App;
